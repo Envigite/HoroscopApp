dd8790f04efb8512d663b73d5cbff77732d532d6
Benjamin Contreras <benjacontrerasma@yahoo.com>
2024-09-24 01:31:20 -0300

    end Lucky Fragment

--- app/src/main/java/com/envigite/horoscopapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/envigite/horoscopapp/ui/luck/LuckFragment.kt
@@ -1,21 +1,37 @@
 package com.envigite.horoscopapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.envigite.horoscopapp.R
 import com.envigite.horoscopapp.databinding.FragmentLuckBinding
+import com.envigite.horoscopapp.ui.core.listeners.OnSwipeTouchListener
+import com.envigite.horoscopapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var randomCardProvider: RandomCardProvider
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,20 +40,118 @@ class LuckFragment : Fragment() {
 
     private fun initUI() {
         initListeners()
+        preparePrediction()
     }
 
+    private fun preparePrediction() {
+        val currentluck = randomCardProvider.getLucky()
+        currentluck?.let { luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener { shareResult(currentPrediction) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners() {
-        binding.ivRoulette.setOnClickListener { spinRoulette() }
+
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()) {
+
+            override fun onSwipeRight() {
+                spinRoulette(true)
+            }
+
+            override fun onSwipeLeft() {
+                spinRoulette(false)
+            }
+        })
     }
 
-    private fun spinRoulette() {
+    private fun spinRoulette(rotation: Boolean) {
         val random = Random()
         val degrees = random.nextInt(1440) + 360
+        val finalDegrees = if (rotation) degrees else -degrees
         val animator =
-            ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
+            ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, finalDegrees.toFloat())
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
         animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                binding.ivReverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                growCard()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.ivReverse.startAnimation(slideUpAnimation)
+
+    }
+
+    private fun growCard() {
+        val growAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.grow)
+
+        growAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.ivReverse.isVisible = false
+                showPremonitionView()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.ivReverse.startAnimation(growAnimation)
+
+    }
+
+    private fun showPremonitionView() {
+        val disappearAnimation = AlphaAnimation(1.0f, 0.0f)
+        disappearAnimation.duration = 200
+
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+        appearAnimation.duration = 1000
+
+        disappearAnimation.setAnimationListener(object : AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.preview.isVisible = false
+                binding.prediction.isVisible = true
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.preview.startAnimation(disappearAnimation)
+        binding.prediction.startAnimation(appearAnimation)
+
     }
 
     override fun onCreateView(
